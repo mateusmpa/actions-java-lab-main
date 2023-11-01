f30800eea0e55e2d287bd69eaca7769f700545a0
Mateus Alves <mateusmpa@gmail.com>
2023-10-31 21:46:47 -0300

    Update App.java

--- src/main/java/org/sen/App.java
+++ src/main/java/org/sen/App.java
@@ -1,13 +1,31 @@
 package org.sen;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println("I am your lab Java application!");
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
-        DeepThought ourSuperComputer = new DeepThought();
-        int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
-        System.out.println(ans);
+public class App {
+    public static void main(String[] args) {
+        // Create a Jetty server that listens on port 10010
+        Server server = new Server(10010);
+
+        // Create a servlet context
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+
+        // Add a servlet that handles incoming requests
+        context.addServlet(new ServletHolder(new MyServlet()), "/*");
+
+        server.setHandler(context);
+
+        try {
+            // Start the server and wait for 5 minutes
+            server.start();
+            server.join();
+            Thread.sleep(5 * 60 * 1000); // Sleep for 5 minutes
+            server.stop(); // Stop the server after 5 minutes
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
